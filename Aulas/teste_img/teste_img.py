aa25cd99981d927c0affbcd431406ca1e1026a29
jeronimopenha <jeronimopenha@gmail.com>
2023-11-18 15:17:12 -0300

    exemplos pra aulas

---
+++ Aulas/teste_img/teste_img.py
@@ -0,0 +1,26 @@
+from PIL import Image
+from math import ceil, log2
+
+im = Image.open("/home/jeronimo/Imagens/teste_.bmp")
+im.load()
+height, widht = im.size
+p = []
+for row in range(height):
+    for col in range(widht):
+        a = im.getpixel((row, col))
+        nr = int(a[0]/255*32)
+        ng = int(a[0]/255*64)
+        nb = int(a[0]/255*32)
+        hex = (nr << 11) | (ng << 5) | nr
+        p.append("%0.4X" % (hex))
+pot_2 = ceil(log2(len(p)))
+diff = pow(2, pot_2) - len(p)
+for i in range(diff):
+    p.append("%0.4X" % (0))
+counter = 0
+with open('/home/jeronimo/Imagens/arch.rom', 'w') as file:
+    for v in p:
+        file.writelines("assign pixel[%d] = 16'h%s;\n" % (counter, v))
+        counter += 1
+
+file.close()
